6ebc2c70cef822faa71e8dfb8bd6f72ca4bff591
Zachary Huang <33015448+zachary62@users.noreply.github.com>
2022-11-02 22:54:30 -0400

    Update server.py

--- webserver/server.py
+++ webserver/server.py
@@ -41,7 +41,7 @@ DB_PASSWORD = "YOUR_DB_PASSWORD_HERE"
 
 DB_SERVER = "w4111.cisxo09blonu.us-east-1.rds.amazonaws.com"
 
-DATABASEURI = "postgresql://"+DB_USER+":"+DB_PASSWORD+"@"+DB_SERVER+"/w4111"
+DATABASEURI = "postgresql://"+DB_USER+":"+DB_PASSWORD+"@"+DB_SERVER+"/proj1part2"
 
 
 #
